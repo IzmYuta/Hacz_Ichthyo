6b7de207073704d8b39f106ed34b359d5622614e
IzmYuta <y.izumi@4nonome.com>
2025-09-17 11:09:43 +0900

    APIおよびホストサービスのテスト実行時に、テストファイルの存在を確認するロジックを追加しました。また、APIおよびホストサービスに基本的なテストファイルを新規作成しました。

---
+++ services/host/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// Basic test to ensure main package can be imported
+	// This is a placeholder test that will pass
+	t.Log("Main package test passed")
+}
